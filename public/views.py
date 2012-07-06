c97acf441561b320ef7795e6fa735161d8f0cf4d
Thiago Avelino <thiagoavelinoster@gmail.com>
2012-07-06 15:46:42 -0300

    Create view home Use render form to simplify the work

---
+++ public/views.py
@@ -0,0 +1,30 @@
+﻿import os
+import time
+import datetime
+import simplejson
+
+from london.templates import render_template
+from london.http import HttpResponse
+from london.http import HttpResponseRedirect
+from london.sse import server_sent_event_source
+from london.apps.ajax.tags import redirect_to
+from london import forms
+
+from models import Votes, SEAWALL
+
+class HomeForm(forms.ModelForm):
+    class Meta:
+        template = 'home'
+        model = Votes
+        fields = ('result')
+        ajax_post_json_response = True
+
+    result = forms.ChoiceField(choices=SEAWALL)
+
+    def ajax_post(self, *args, **kwargs):
+        return self.http_post(*args, **kwargs)
+
+    def http_post(self, *args, **kwargs):
+        if not self.errors:
+            return redirect_to(self.request, '/')
+
